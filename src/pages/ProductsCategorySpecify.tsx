3a623a8961d50b584c4fc8382ca12650b817d8ac
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-08-19 12:22:48 -0500

    create new pages single product and category specific product

---
+++ src/pages/ProductsCategorySpecify.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductsCategorySpecify = () => {
+  return (
+    <div>
+      <h3>Category</h3>
+    </div>
+  )
+}
+
+export default ProductsCategorySpecify
---
+++ src/pages/SingleProduct.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleProduct = () => {
+  return (
+    <div>
+      <h3>Single Product</h3>
+    </div>
+  )
+}
+
+export default SingleProduct
