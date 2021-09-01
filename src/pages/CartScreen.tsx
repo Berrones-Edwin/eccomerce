8095d04954974f6e816eed38d1213ee046b6ab01
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-09-01 16:18:37 -0500

    [Feature] Show list products in cart component

--- src/pages/CartScreen.tsx
+++ src/pages/CartScreen.tsx
@@ -1,7 +1,63 @@
 import React from 'react'
+import useCart from '../hooks/useCart'
 
 const CartScreen = () => {
-  return <h3>Cart Screen</h3>
+  const { cartProducts, setCartProducts } = useCart()
+
+  const handleDeleteProductCart = ({ id }: { id: number }) => {
+    const newCartProducts = cartProducts.filter((product) => product.id !== id)
+    setCartProducts(newCartProducts)
+  }
+  let totalPriceProducts = 0
+
+  cartProducts.forEach(
+    (product) =>
+      (totalPriceProducts = totalPriceProducts + Number(product.price))
+  )
+
+  if (cartProducts.length === 0) return <p>The cart is empty!!!</p>
+  return (
+    <>
+      <h3>Cart Screen</h3>
+      <table>
+        <tbody>
+          {cartProducts.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <img
+                  height="100"
+                  width="100"
+                  src={product.image}
+                  alt={product.title}
+                />
+              </td>
+              <td colSpan={3}>
+                <p>{product.title}</p>
+                <small>{product.category}</small>
+              </td>
+              <td>
+                <p>{product.price}</p>
+              </td>
+              <td>
+                <button
+                  onClick={() => handleDeleteProductCart({ id: product.id })}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+          <tr>
+            <td align="right" colSpan={3}>
+              <p>
+                Total <b>{totalPriceProducts}</b>{' '}
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
 }
 
 export default CartScreen
