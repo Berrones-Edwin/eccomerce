0ca464fabcce9e714f00c6d44f6da581839aa1b0
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-09-24 15:47:41 -0500

    [Feature] Home screen finish

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -110,60 +110,53 @@ const NotificationIcon = createIcon({
 
 export default function NewsLetter() {
   return (
-    <Flex
-      minH={'100vh'}
+    <Stack
+      boxShadow={'2xl'}
+      bg={useColorModeValue('white', 'gray.700')}
+      rounded={'xl'}
+      p={10}
+      spacing={8}
       align={'center'}
-      justify={'center'}
-      py={12}
-      bg={useColorModeValue('gray.50', 'gray.800')}
+      mb={'3.5rem'}
     >
-      <Stack
-        boxShadow={'2xl'}
-        bg={useColorModeValue('white', 'gray.700')}
-        rounded={'xl'}
-        p={10}
-        spacing={8}
-        align={'center'}
-      >
-        <Icon as={NotificationIcon} w={24} h={24} />
-        <Stack align={'center'} spacing={2}>
-          <Heading
-            textTransform={'uppercase'}
-            fontSize={'3xl'}
-            color={useColorModeValue('gray.800', 'gray.200')}
-          >
-            Subscribe to Newsletters
-          </Heading>
-          <Text fontSize={'lg'} color={'gray.500'}>
-            Be aware of upcoming sales and events. Receive gifts and special
-            offters.
-          </Text>
-        </Stack>
-        <Stack spacing={4} direction={{ base: 'column', md: 'row' }} w={'full'}>
-          <Input
-            type={'text'}
-            placeholder={'john@doe.net'}
-            color={useColorModeValue('gray.800', 'gray.200')}
-            bg={useColorModeValue('gray.100', 'gray.600')}
-            rounded={'full'}
-            border={0}
-            _focus={{
-              bg: useColorModeValue('gray.200', 'gray.800'),
-              outline: 'none'
-            }}
-          />
-          <Button
-            bg={'blue.400'}
-            rounded={'full'}
-            color={'white'}
-            flex={'1 0 auto'}
-            _hover={{ bg: 'blue.500' }}
-            _focus={{ bg: 'blue.500' }}
-          >
-            Subscribe
-          </Button>
-        </Stack>
+      <Icon as={NotificationIcon} w={24} h={24} />
+      <Stack align={'center'} spacing={2}>
+        <Heading
+          textTransform={'uppercase'}
+          fontSize={'3xl'}
+          color={useColorModeValue('gray.800', 'gray.200')}
+        >
+          Subscribe to Newsletters
+        </Heading>
+        <Text fontSize={'lg'} color={'gray.500'}>
+          Be aware of upcoming sales and events. Receive gifts and special
+          offters.
+        </Text>
       </Stack>
-    </Flex>
+      <Stack spacing={4} direction={{ base: 'column', md: 'row' }} w={'full'}>
+        <Input
+          type={'text'}
+          placeholder={'john@doe.net'}
+          color={useColorModeValue('gray.800', 'gray.200')}
+          bg={useColorModeValue('gray.100', 'gray.600')}
+          rounded={'full'}
+          border={0}
+          _focus={{
+            bg: useColorModeValue('gray.200', 'gray.800'),
+            outline: 'none'
+          }}
+        />
+        <Button
+          bg={'blue.400'}
+          rounded={'full'}
+          color={'white'}
+          flex={'1 0 auto'}
+          _hover={{ bg: 'blue.500' }}
+          _focus={{ bg: 'blue.500' }}
+        >
+          Subscribe
+        </Button>
+      </Stack>
+    </Stack>
   )
 }
