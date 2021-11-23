ffaf8690b44ec080ee836e11b5b70bbb681f3d7d
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-11-23 12:40:16 -0600

    [Feature] Implement lazy loading

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,21 +1,22 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import PrivateRoute from './PrivateRoute'
-
-import HomeScreen from '../pages/HomeScreen'
-import SingleProductScreen from '../pages/SingleProductScreen'
-import ProductsCategorySpecify from '../pages/ProductsCategorySpecifyScreen'
-
-import LoginScreen from '../pages/LoginScreen'
-import Error404Screen from '../pages/Error404Screen'
-import WishListScreen from '../pages/WishListScreen'
-import ProductsScreen from '../pages/ProductsScreen'
-
 import PublicRoute from './PublicRoute'
 import Navbar from '../components/Navbar'
 import { UserContext } from '../context/UserContextProvider'
 import { useUser } from '../hooks/useUser'
+
+const HomeScreen = lazy(() => import('../pages/HomeScreen'))
+const SingleProductScreen = lazy(() => import('../pages/SingleProductScreen'))
+const ProductsCategorySpecify = lazy(
+  () => import('../pages/ProductsCategorySpecifyScreen')
+)
+const LoginScreen = lazy(() => import('../pages/LoginScreen'))
+const Error404Screen = lazy(() => import('../pages/Error404Screen'))
+const WishListScreen = lazy(() => import('../pages/WishListScreen'))
+const ProductsScreen = lazy(() => import('../pages/ProductsScreen'))
+const SettingsScreen = lazy(() => import('../pages/SettingsScreen'))
 
 const AppRoutes = () => {
   const { isLoggen } = useUser()
@@ -27,29 +28,37 @@ const AppRoutes = () => {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={HomeScreen} />
-        <Route path="/products" exact component={ProductsScreen} />
-        <Route path="/products/:id" exact component={SingleProductScreen} />
-        <Route
-          path="/products/category/:name"
-          exact
-          component={ProductsCategorySpecify}
-        />
-        <PrivateRoute
-          isAuthenticated={isLoggen}
-          path="/wishlist"
-          exact
-          component={WishListScreen}
-        />
-        <PublicRoute
-          isAuthenticated={isLoggen}
-          path="/login"
-          exact
-          component={LoginScreen}
-        />
-        <Route path="**" component={Error404Screen} />
-      </Switch>
+      <Suspense fallback={'...loading'}>
+        <Switch>
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/products" exact component={ProductsScreen} />
+          <Route path="/products/:id" exact component={SingleProductScreen} />
+          <Route
+            path="/products/category/:name"
+            exact
+            component={ProductsCategorySpecify}
+          />
+          <PrivateRoute
+            isAuthenticated={isLoggen}
+            path="/wishlist"
+            exact
+            component={WishListScreen}
+          />
+          <PrivateRoute
+            isAuthenticated={isLoggen}
+            path="/settings"
+            exact
+            component={SettingsScreen}
+          />
+          <PublicRoute
+            isAuthenticated={isLoggen}
+            path="/login"
+            exact
+            component={LoginScreen}
+          />
+          <Route path="**" component={Error404Screen} />
+        </Switch>
+      </Suspense>
     </Router>
   )
 }
