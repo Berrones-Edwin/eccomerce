51ddf5d6f67a8bb17dd0fd83d931d082367af9d2
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-11-25 15:56:31 -0600

    [Feature] Edit screen order history

--- src/pages/OrderHistory.tsx
+++ src/pages/OrderHistory.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Stack,
+  Heading,
+  Image,
+  Button
+} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const OrderHistory = () => {
   return (
-    <div>
-      <h3>Order History</h3>
-    </div>
+    <Tabs>
+      <TabList>
+        <Tab>Order History</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>
+          <Stack
+            direction={'column'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            h={'100%'}
+            spacing={3}
+          >
+            <Image width={'300'} height={'300'} src={'/empty-cart.svg'} />
+            <Heading size={'lg'}>You do not have orders yet</Heading>
+            <Button as={Link} to="/">
+              Buy now!
+            </Button>
+          </Stack>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   )
 }
 
